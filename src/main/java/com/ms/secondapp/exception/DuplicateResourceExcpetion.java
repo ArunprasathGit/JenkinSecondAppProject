c26413d835efa6a62fdc3b851e596f04e10684d7
Arunprasath_A <girisktt@gmail.com>
2026-04-27 14:17:48 +0530

    Exception Handling

--- src/main/java/com/ms/secondapp/exception/DuplicateResourceExcpetion.java
+++ src/main/java/com/ms/secondapp/exception/DuplicateResourceExcpetion.java
@@ -3,5 +3,6 @@ package com.ms.secondapp.exception;
 public class DuplicateResourceExcpetion extends RuntimeException {
     public DuplicateResourceExcpetion(String message) {
         super(message);
+
     }
 }
